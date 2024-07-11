deb207a3b4e4bb4b15ef80ec256e0f0a304ebae3
ajscodes <ayushmaradia@gmail.com>
2024-07-11 22:20:54 +0530

    let, const & var

---
+++ Master JS/variables.js
@@ -0,0 +1,12 @@
+//about const, let, var or without
+const stuID = 12345;
+let stuEmail = "stu2google.com"
+var stuCity = "Indore"
+stuMob = 8564123784
+
+//in const we can't change but in other 3 we can able to change
+/*
+prefer not to use var because of issue in function and block scope
+*/
+
+console.table([stuID, stuEmail, stuCity, stuMob])
