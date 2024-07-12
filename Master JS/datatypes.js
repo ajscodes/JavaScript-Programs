96ebc5c0f539c5779ab6c92fd567dfa6146df0d2
ajscodes <ayushmaradia@gmail.com>
2024-07-12 22:08:05 +0530

    dataTypes in js

---
+++ Master JS/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //(optional) 
+//it's indicate javascript to use newer version of js.
+
+//number => 2 to power 53
+//string => ""
+//bigint
+//boolean => true/false
+//undefined => 
+//null => datatype & standalone value
+//symbol => unique
+
+let name = "Tishant"
+let age = 19
+let isLoggedIn = true
+let state;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof undefined);
+
+//in undefined we can get undefined but in null we can get object 
+//it's not an issue
