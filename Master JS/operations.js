fc83ec5f16a34c3452806f2f9b9e22e8735fa8fc
ajscodes <ayushmaradia@gmail.com>
2024-07-14 17:19:44 +0530

    operations & rules

---
+++ Master JS/operations.js
@@ -0,0 +1,18 @@
+console.log(2+2);
+console.log(2**4);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Jolly"
+let str3 = str1 + str2
+
+console.log("1" + 2); // o/p: 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32 = (1+2)2
+//String to number conversion rules are reserved acc to ECMA
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
