eb030f3d227e24c1b498d5daf49fd13026501054
ajscodes <ayushmaradia@gmail.com>
2024-08-16 16:35:19 +0530

    operations & rules

--- Master JS/operations.js
+++ Master JS/operations.js
@@ -10,7 +10,13 @@ console.log("1" + 2); // o/p: 12
 console.log(1 + "2"); // 12
 console.log("1" + 2 + 2); //122
 console.log(1 + 2 + "2"); //32 = (1+2)2
+console.log(1+4+5+6+"76"); //1676
+console.log("76"+1+4+5+6); //761456
+console.log("76"+1+4+5+6 + "45" + 1+2+3+6);//761456451236
 //String to number conversion rules are reserved acc to ECMA
+//if string come first then direct concate without operation
+// but if string is at last position then operation before string performed and it concate with string
+
 
 let gameCounter = 100
 ++gameCounter;
